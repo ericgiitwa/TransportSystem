c406b04a14a39647fe4250523b3b55d571725f14
ericgiitwa <ericgiitwa73@gmail.com>
2026-02-23 20:36:22 +0300

    createdthe vihicle class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+                Vehicle myCar = new Vehicle("Midnight Blue", "Tesla", "Model 3",
+                "Sedan", "ABC-1234", 987654, 5);
+
+
+        myCar.displayDetails();
+
+    }
+}
