7b963271f1c93a88fa9e3b64b8ad5350234985cf
ericgiitwa <ericgiitwa73@gmail.com>
2026-03-03 19:15:32 +0300

    added a check and get  availability for drivers to confirm if they are available for a ride

--- src/Driver.java
+++ src/Driver.java
@@ -1,18 +1,35 @@
 public class Driver extends BaseUser {
     private boolean available = true;
     private double rating;
+    private Car mycar;
 
     public Driver(String name, String phone, int id, int age, double rating) {
         super(name, phone, age);
         this.rating = rating;
+        this.mycar = mycar;
+    }
+
+    public Car getMyCar() {
+        return this.mycar;
     }
 
     @Override
     public void confirmRide() {
-        System.out.println("Driver " + getName() + " has accepted the request.");
+        System.out.println("Driver " + getName() + " accepted.");
     }
 
-    public void setAvailable(boolean status) {
+    public void setAvailability(boolean status) {
         this.available = status;
     }
+
+    public boolean getAvailable(){return available;}
+
+    public void checkAvailability() {
+        if (this.available = true) {
+            System.out.println("You can select a new passanger to transport");
+        } else {
+            System.out.println("Complete the current ride first!");
+        }
+    }
+
 }
