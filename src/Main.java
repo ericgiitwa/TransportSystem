acacf39f3bf2d196b7bd11334a757b320e215329
ericgiitwa <ericgiitwa73@gmail.com>
2026-03-03 20:05:53 +0300

    added a menu option for login..will create case statements for each

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,42 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
         System.out.println("WELCOME TO OUR TRANSPORTATION APP");
+        System.out.println("Select your Account:");
+        System.out.println("1. Passenger");
+        System.out.println("2. Driver");
+
+        String accountChoice = scanner.nextLine();
+        switch (accountChoice){
+            case "Passenger":
+                System.out.println("HELLO Passenger");
+                System.out.println("create new account");
+                System.out.println("Sign in");
+                System.out.println("View wallet");
+                System.out.println("View rating");
+
+            case "Driver":
+                System.out.println("HELLO Driver");
+                System.out.println("create new account");
+                System.out.println("Sign in");
+                System.out.println("View wallet");
+                System.out.println("View rating");
+
+        }
+
+        scanner.close();
+
+
+
+
+
 
         Passenger p1 = new Passenger("Lenny Chingi", "0711223344", 194072, 24, 3.44);
         Passenger p2 = new Passenger("Jason Kangogo", "0722334455", 200316, 17, 4.66);
 
-        Car taxi1 = new Car("Toyota Fielder", "KDL 123X", "Silver", "Green", 4);
+        Car car1 = new Car("Toyota Fielder", "KDL 123X", "Silver", "Green", 4);
         Driver d1 = new Driver("Eric Giitwa", "0733445566", 200857, 23, 5.00);
 
         p1.setLocation("Nairobi CBD");
@@ -13,10 +44,13 @@ public class Main {
 
         p1.getLocation();
         d1.getLocation();
+        d1.confirmRide();
 
         p1.verifyAge();
         d1.verifyAge();
 
+        d1.setAvailability(true);
+        d1.checkAvailability();
 
     }
 }
