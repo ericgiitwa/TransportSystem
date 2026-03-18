6289beddb67e5067e2f0890c3bf4546b088a3184
ericgiitwa <ericgiitwa73@gmail.com>
2026-03-18 20:14:01 +0300

    added a scanner option into the car class for drivers to be able to register their cars

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Car {
         private String brand;
         private String model;
@@ -5,18 +8,36 @@ public class Car {
         private String color;
         private int capacity;
 
-        public Car(String brand, String model, String regPlate, String color, int capacity) {
-            this.brand = brand;
-            this.model = model;
-            this.regPlate = regPlate;
-            this.color = color;
-            this.capacity = capacity;
+//        public Car(String brand, String model, String regPlate, String color, int capacity) {
+//            this.brand = brand;
+//            this.model = model;
+//            this.regPlate = regPlate;
+//            this.color = color;
+//            this.capacity = capacity;
+//        }
+
+    public static void carRegistration(){
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("INPUT YOUR VEHICLES BRAND:");
+            String brand = scanner.nextLine();
+            System.out.println("INPUT YOUR VEHICLES model:");
+            String model = scanner.nextLine();
+            System.out.println("INPUT YOUR VEHICLES regPlate:");
+            String regPlate = scanner.nextLine();
+            System.out.println("INPUT YOUR VEHICLES color:");
+            String color = scanner.nextLine();
+            System.out.println("your vehicle details are: "+color + " " + brand + model + " " + regPlate + "");
+        }
+        catch (InputMismatchException e) {}
+        finally{
+            scanner.close();
         }
 
-        // Getters to allow other classes to see the car info
-        public String getCarDetails() {
-            return color + " " + brand + model + " " + regPlate + "";
-        }
+    }
+//        public String getCarDetails() {
+//            return color + " " + brand + model + " " + regPlate + "";
+//        }
     }
 
 
